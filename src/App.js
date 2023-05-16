a17d38614cf6d04e0493fb50f41d372a52369413
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2023-05-16 23:53:22 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
-App.js
+import "./index.css";
+import Lists from "./Components/List_and_keys/Lists";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Groceries Super Store!!!</h1>
+      <Lists />
+    </div>
+  );
+}
+
+export default App;
+
+// npm i react-icons -D
